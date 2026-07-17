26c12c004d3658a2ff4db0c8aa561dae19d5f855
strrev-dev <deepanmaurya699@gmail.com>
2026-07-17 21:11:56 +0530

    sum basics program

---
+++ SumBasics.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class SumBasics {
+
+    public static void main(String args[]) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter number A : ");
+        int A = scan.nextInt();
+        System.out.print("Enter number B : ");
+        int B = scan.nextInt();
+        int Sum = A+B;
+        System.out.println("Sum of A and B is : " + Sum);
+
+    }
+}
