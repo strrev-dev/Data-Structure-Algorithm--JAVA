ae37bc8f00ef209a5b421fd79b5304ee4814a9f8
strrev-dev <deepanmaurya699@gmail.com>
2026-07-21 22:31:11 +0530

    Print 1 to 100 numbers using while loop

---
+++ Print1to100.java
@@ -0,0 +1,12 @@
+
+public class Print1to100 {
+
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 100) {
+            System.out.print( " " + count);
+            count++;
+        }
+    }
+
+}
