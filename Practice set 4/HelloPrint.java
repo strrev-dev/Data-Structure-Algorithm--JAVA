0613a41d2a4595a809a5032c061ba5a3d91bf965
strrev-dev <deepanmaurya699@gmail.com>
2026-07-26 21:38:59 +0530

    checking how many times the Hello will print in following program

---
+++ Practice set 4/HelloPrint.java
@@ -0,0 +1,12 @@
+//Ques 1: How many times will the following code print "Hello"?
+
+public class HelloPrint {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
+            i += 2;
+        }
+    }
+}
+
+
