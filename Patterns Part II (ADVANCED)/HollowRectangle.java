fc345e56954cccb4ad07100b2b5f51a92be64a2b
strrev-dev <deepanmaurya699@gmail.com>
2026-08-11 19:46:33 +0530

    Hollow Rectangle pattern

---
+++ Patterns Part II (ADVANCED)/HollowRectangle.java
@@ -0,0 +1,23 @@
+
+public class HollowRectangle {
+
+    public static void printhollowrectangle(int totrows, int totcols) {
+
+        for (int i = 1; i <= totrows; i++) {
+            for (int j = 1; j <= totcols; j++) {
+                if (i == 1 || i == totrows || j == 1 || j == totcols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hollow Rectangle :");
+        printhollowrectangle(5, 15);
+
+    }
+}
