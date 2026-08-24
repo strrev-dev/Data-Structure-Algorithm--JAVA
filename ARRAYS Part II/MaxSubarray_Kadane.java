5e4fcd522543713548b4c4b3e252af2fb1edfb1b
strrev-dev <deepanmaurya699@gmail.com>
2026-08-24 18:48:12 +0530

    Kadane's algo for max subarray sum if there exist atleast one +ve integer

--- ARRAYS Part II/MaxSubarray_Kadane.java
+++ ARRAYS Part II/MaxSubarray_Kadane.java
@@ -1,5 +1,24 @@
 
-
 public class MaxSubarray_Kadane {
-    
+
+    public static void Kadane_algo(int numbers[]) {
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            currSum = currSum + numbers[i];
+            if (currSum < 0) {
+                currSum = 0;
+            }
+            maxSum = Math.max(currSum, maxSum);
+        }
+        System.out.println("Max Sum is : " + maxSum);
+    }
+
+    public static void main(String[] args) {
+
+        int numbers[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        Kadane_algo(numbers);
+    }
 }
--- ARRAYS Part II/MaxSubarray_Prefix.java
+++ ARRAYS Part II/MaxSubarray_Prefix.java
@@ -1,32 +1,33 @@
 
-
 public class MaxSubarray_Prefix {
-    public static void prefixSummation(int numbers[]){
+
+    public static void prefixSummation(int numbers[]) {
 
         int currSum = 0;
-        int prefix[] = new int [numbers.length];
+        int prefix[] = new int[numbers.length];
         int maxSum = Integer.MIN_VALUE;
-         prefix[0] = numbers[0];
+        prefix[0] = numbers[0];
 
-         for(int i= 1; i< prefix.length; i++){
-            prefix[i] = prefix[i-1]+ numbers[i] ;
-         }
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
 
-         for(int i = 0; i <numbers.length; i++){
-             int start = i;
-             for(int j =i; j < numbers.length; j++){
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i;
+            for (int j = i; j < numbers.length; j++) {
                 int end = j;
-                currSum = start == 0 ? prefix[end]: prefix[end] -prefix[start-1];
-                if(maxSum < currSum){
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start - 1];
+                if (maxSum < currSum) {
                     maxSum = currSum;
                 }
-             }
-         }
-         System.out.println("Maximum Sum  : " + maxSum);
+            }
+        }
+        System.out.println("Maximum Sum  : " + maxSum);
     }
-    public static void main(String[] args){
 
-        int numbers[] = {1,-2,6,-1,3};
+    public static void main(String[] args) {
+
+        int numbers[] = {1, -2, 6, -1, 3};
         prefixSummation(numbers);
     }
 }
