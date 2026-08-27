857970504d4cc770c849ba6e7c482908e320d1c6
strrev-dev <deepanmaurya699@gmail.com>
2026-08-27 17:37:23 +0530

    O(n) solution for the duplicate check in the array

--- ARRAYS Part II/Assignment Question/Arrayscodefirst.java
+++ ARRAYS Part II/Assignment Question/Arrayscodefirst.java
@@ -1,3 +1,6 @@
+
+import java.util.HashSet;
+
 /* Question 1. given an integer array nums, return true if any value appears at least twice  in the array and return false if every element is distinct */
 public class Arrayscodefirst {
 
@@ -17,9 +20,24 @@ public class Arrayscodefirst {
         return false;
     }
 
+    //hashset method O(n) complexity
+    public static boolean duplicate(int nums[]) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+
+    }
+
     public static void main(String[] args) {
-        int nums[] = {1, 2, 3, 11, 11};
-        System.out.print("The element in the arrray repeated? : " + simdis(nums));
+        int nums[] = {1, 2, 3, 11};
+        // System.out.print("The element in the arrray repeated? : " + simdis(nums));
+        System.out.print("The element in the arrray repeated? : " + duplicate(nums));
 
     }
 }
