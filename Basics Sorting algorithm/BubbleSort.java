3075c312abded7e1742643c38da044fc045da2bb
strrev-dev <deepanmaurya699@gmail.com>
2026-09-09 19:19:11 +0530

    optimised bubble sort

--- Basics Sorting algorithm/BubbleSort.java
+++ Basics Sorting algorithm/BubbleSort.java
@@ -3,15 +3,20 @@ public class BubbleSort {
 
     public static void bubbleAlgo(int[] numbers, int n) {
         for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
             for (int j = 0; j < n - 1 - i; j++) {
                 if (numbers[j] > numbers[j + 1]) {
                     int temp = numbers[j];
                     numbers[j] = numbers[j + 1];
                     numbers[j + 1] = temp;
+                    swapped = true;
 
                 }
             }
-        }       
+            if (!swapped) {
+                break;
+            }
+        }
 
     }
 
@@ -24,7 +29,9 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int numbers[] = {5, 1, 4, 2, 8};
+
+        // int numbers[] = {5, 1, 4, 2, 8};
+        int numbers[] = {1,2,3,4,5};
         int n = numbers.length;
 
         bubbleAlgo(numbers, n);
