3d002af06e73f936f35d94941ad0b34cb17ac68d
strrev-dev <deepanmaurya699@gmail.com>
2026-09-18 20:47:28 +0530

    loop correction insertion sort

--- Basics Sorting algorithm/insertionSort.java
+++ Basics Sorting algorithm/insertionSort.java
@@ -2,7 +2,7 @@
 public class insertionSort {
 
     public static void insertionAlgo(int arr[], int n) {
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < n ; i++) {
             int j = i;
             while (j > 0 && arr[j] < arr[j - 1]) {
                 int temp = arr[j];
@@ -20,7 +20,7 @@ public class insertionSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {2, 1, 4, 3, 5};
+        int arr[] = {2, 1, 7,4,0, 3, 5};
         int n = arr.length;
         insertionAlgo(arr, n);
         printArray(arr, n);
